3092cc96ef2e4546601b48af380dfa3a9124b0cc
Saloni Sharma <sharmasaloni061@gmail.com>
2019-01-22 20:30:04 +0530

    added author name to stories

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const Story = ({title,id}) => (
-    <li> <Link to={`/story/${id}`}>{title}</Link></li>
+const Story = ({title,id,author}) => (
+    <li> <Link to={`/story/${id}`}>{title}</Link> - {author}</li>
 )
 
 
@@ -12,7 +12,7 @@ class Stories extends React.Component {
             <div>
                 <ul>
                     {this.props.hits.map((value,index)=>{
-                            return <Story key={index} title={value.title} id={value.objectID} />
+                            return <Story key={index} title={value.title} id={value.objectID} author={value.author} />
                     })}
                 </ul>
             </div>
